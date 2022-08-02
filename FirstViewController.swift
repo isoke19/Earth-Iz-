c5dd73855bd79259698dffc429cef3ec6b44eb38
Khalifah <42419365+isoke19@users.noreply.github.com>
2022-08-02 12:38:09 -0700

    Add files via upload

---
+++ FirstViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FirstViewController.swift
+//  Khali's  prototype app
+//
+//  Created by Muhammad,Khalifah on 1/23/18.
+//  Copyright © 2018 Muhammad,Khalifah. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a n
+     
+        print("view 2 BOOMPAYAH")
+        
+        func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+  
+        print("view 1 BOOMPAYAH")
+    
+}
+
+
+}
---
+++ SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Khali's  prototype app
+//
+//  Created by Muhammad,Khalifah on 1/23/18.
+//  Copyright © 2018 Muhammad,Khalifah. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
